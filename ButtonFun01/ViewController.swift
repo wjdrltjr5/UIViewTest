44ac6fa52861cd4b1e013d7c8b6415daafbfdd26
dit08 <dit@imac-3.local>
2019-03-13 15:38:39 +0900

    눌렀을때 색깔이 바뀜

--- ButtonFun01/ViewController.swift
+++ ButtonFun01/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var myLabel: UILabel!
     @IBOutlet weak var mySubView: UIView!
+    var checked=false; //처음 클릭안했을때를.false 로 설정 했을떄는. true
     
+    //일반함수
+    //jhkim3217@gmail.com
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,7 +26,21 @@ class ViewController: UIViewController {
         myLabel.text = "Hello Iphone!!!"
        mySubView.backgroundColor = UIColor.red
     }
-
-
+    //액션함수 눌렀을떄 색깔이 바뀜
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        if(checked == false){
+            mySubView.backgroundColor=UIColor.blue;
+            checked = true;
+        }
+        else if(checked == true){
+            mySubView.backgroundColor=UIColor.red;
+            checked = false;
+        }
+       
+        
+    }
+    
+    
 }
 
